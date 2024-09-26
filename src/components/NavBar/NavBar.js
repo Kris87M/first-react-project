f83c416a6025503c2641c1d6d1fe3437e5221be8
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2024-09-26 22:00:52 +0200

    Use NavLink to style active page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,17 @@
 import styles from './NavBar.module.scss';
 import Container from '../Container/Container'
+import { Link, NavLink } from 'react-router-dom';
   
 const NavBar = () => {
   return (
     <nav>
       <Container className={styles.navContainer}>
         <div className={styles.wrapper}>
-          <a href='/'><span className='fa fa-tasks' /></a>
+          <Link to="/"><span className='fa fa-tasks' /></Link>
           <ul className={styles.navlist}>
-            <li className={styles.navlink}><a href='/'>Home</a></li>
-            <li className={styles.navlink}><a href='/favorite'>Favorite</a></li>
-            <li className={styles.navlink}><a href='/about'>About</a></li>
+            <li className={styles.navlink}><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/">Home</NavLink></li>
+            <li className={styles.navlink}><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/favorite">Favorite</NavLink></li>
+            <li className={styles.navlink}><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/about">About</NavLink></li>
           </ul>
         </div>  
       </Container>
