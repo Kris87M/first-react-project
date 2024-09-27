f40c8975c6bf012e9d4e047b9499e1abcf5d461f
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2024-09-28 00:07:47 +0200

    Add new list option (create ListForm component)

--- src/redux/store.js
+++ src/redux/store.js
@@ -19,6 +19,7 @@ export const getAllLists = (state => state.lists);
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
 export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload });
+export const addList = payload => ({ type: 'ADD_LIST', payload });
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -28,6 +29,8 @@ const reducer = (state, action) => {
       return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }] };
     case 'UPDATE_SEARCHSTRING':
       return { ...state, searchString: action.payload.title };
+    case 'ADD_LIST':
+      return { ...state, lists: [...state.lists, { ...action.payload, id: shortid() }] };
     default:
       return state;
   };
