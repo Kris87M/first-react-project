b3e483e4ad9f8354a979d625e8483cac4d1daff8
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2024-09-27 23:23:35 +0200

    Add multiple list (create Lists component)

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,13 +2,17 @@ import styles from './SearchForm.module.scss'
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 import { useDispatch } from 'react-redux';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { updateSearchString } from '../../redux/store';
 
 const SearchForm = () => {
 
     const [title, setTitle] = useState('');
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(updateSearchString({ title: '' }));
+    }, [dispatch]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
